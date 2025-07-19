31f67ce91ba3c925678c297bb33ca0d698771671
tohidnedariya78-ops <tohidnedariya78@gmail.com>
2025-07-19 19:05:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-"scripts": {
-  "dev": "next dev",
-  "build": "next build && next export",
-  "start": "next start"
-}// next.config.js
+// next.config.js
 module.exports = {
   output: 'export',
 };
